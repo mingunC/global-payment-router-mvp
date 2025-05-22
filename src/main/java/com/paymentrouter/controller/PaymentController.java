abae101b8e6f264e7dc34f68f7aa43531a758966
mingunC <mingun.ryan.choi@gmail.com>
2025-05-22 00:15:04 -0400

    Add REST controller for payment processing API, Dynamo repository for transaction storage

--- src/main/java/com/paymentrouter/controller/PaymentController.java
+++ src/main/java/com/paymentrouter/controller/PaymentController.java
@@ -1,4 +1,23 @@
 package com.paymentrouter.controller;
 
+import com.paymentrouter.model.PaymentRequest;
+import com.paymentrouter.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payments")
 public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping("/process")
+    public ResponseEntity<String> processPayment(@RequestBody PaymentRequest request) {
+        String result = paymentService.processPayment(request);
+        return ResponseEntity.ok(result);
+    }
 }
--- src/main/java/com/paymentrouter/repostiory/DynamoDBRepository.java
+++ src/main/java/com/paymentrouter/repostiory/DynamoDBRepository.java
@@ -1,4 +1,25 @@
 package com.paymentrouter.repostiory;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
 public class DynamoDBRepository {
+    @Autowired
+    private AmazonDynamoDB amazonDynamoDB;
+
+    public void savePayment(String senderId, String receiverId, double amount) {
+        Map<String, AttributeValue> item = new HashMap<>();
+        item.put("senderId", new AttributeValue().withS(senderId));
+        item.put("receiverId", new AttributeValue().withS(receiverId));
+        item.put("amount", new AttributeValue().withN(String.valueOf(amount)));
+        PutItemRequest request = new PutItemRequest().withTableName("Payments").withItem(item);
+        amazonDynamoDB.putItem(request);
+    }
 }
